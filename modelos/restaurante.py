3f8edb190d3dc21c48b940066510329fe92bbc05
JhonataFerreiraJFL <jhonataferreirajfl@gmail.com>
2024-02-25 13:25:48 -0300

    testes para python poo

---
+++ modelos/restaurante.py
@@ -0,0 +1,28 @@
+class Restaurante:
+    resturantes = []
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.resturantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for resturante in Restaurante.resturantes:
+            print(f'{resturante.nome} | {resturante.categoria} |{resturante.ativo}')
+
+    @property
+    def ativo(self):
+        return 'verdadeiro' if self.ativo else 'false'
+        
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
+
+
+
+
+
+Restaurante.listar_restaurantes()
