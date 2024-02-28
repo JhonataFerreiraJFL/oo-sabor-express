3bc6083c0cc4cc07526735ed55c36c39139bf126
JhonataFerreiraJFL <jhonataferreirajfl@gmail.com>
2024-02-27 21:23:29 -0300

    inciando trabalho com FastAPI

--- app.py
+++ app.py
@@ -1,17 +1,48 @@
-from modelos.restaurante import Restaurante
+
+
+
+"""from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça','Gourmet')
 bebida_suco = Bebida('Suco de melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('paozinho', 2.0, 'o melhor pao da cidade')
-
-restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-restaurante_praca.adcionar_prato_no_cardapio(prato_paozinho)
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
-    main()
+    main()"""
+
+import requests
+import json
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+response = requests.get(url)
+print(response)
+
+if response.status_code == 200:
+    dados_json = response.json()
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+
+        dados_restaurante[nome_do_restaurante].append({
+            "item":item['Item'],
+            "price":item['price'],
+            "description":item['description']
+        })            
+else:
+    print(f'O erro foi {response.status_code}')
+
+for nome_do_restaurante, dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
